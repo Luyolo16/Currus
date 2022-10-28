78ec048f0b99e25965d9a49eff964d4d68408b76
Luyolo16 <Ithinkthereforeheis@gmail.com>
2022-10-28 21:09:20 +0200

    initial commit

---
+++ Upper_Clone/obj/Debug/90/android/src/crc642d84e982e10dce64/RequestDriver.java
@@ -0,0 +1,63 @@
+package crc642d84e982e10dce64;
+
+
+public class RequestDriver
+	extends android.support.v4.app.DialogFragment
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onCreateView:(Landroid/view/LayoutInflater;Landroid/view/ViewGroup;Landroid/os/Bundle;)Landroid/view/View;:GetOnCreateView_Landroid_view_LayoutInflater_Landroid_view_ViewGroup_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("Upper_Clone.Fragments.RequestDriver, Upper_Clone", RequestDriver.class, __md_methods);
+	}
+
+
+	public RequestDriver ()
+	{
+		super ();
+		if (getClass () == RequestDriver.class)
+			mono.android.TypeManager.Activate ("Upper_Clone.Fragments.RequestDriver, Upper_Clone", "", this, new java.lang.Object[] {  });
+	}
+
+	public RequestDriver (double p0)
+	{
+		super ();
+		if (getClass () == RequestDriver.class)
+			mono.android.TypeManager.Activate ("Upper_Clone.Fragments.RequestDriver, Upper_Clone", "System.Double, mscorlib", this, new java.lang.Object[] { p0 });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public android.view.View onCreateView (android.view.LayoutInflater p0, android.view.ViewGroup p1, android.os.Bundle p2)
+	{
+		return n_onCreateView (p0, p1, p2);
+	}
+
+	private native android.view.View n_onCreateView (android.view.LayoutInflater p0, android.view.ViewGroup p1, android.os.Bundle p2);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ Upper_Clone/obj/Debug/90/android/src/crc64e8027dcf2f2a0eab/RegisterationActivity.java
@@ -0,0 +1,86 @@
+package crc64e8027dcf2f2a0eab;
+
+
+public class RegisterationActivity
+	extends android.support.v7.app.AppCompatActivity
+	implements
+		mono.android.IGCUserPeer,
+		com.google.android.gms.tasks.OnSuccessListener,
+		com.google.android.gms.tasks.OnFailureListener,
+		com.google.android.gms.tasks.OnCompleteListener
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onRequestPermissionsResult:(I[Ljava/lang/String;[I)V:GetOnRequestPermissionsResult_IarrayLjava_lang_String_arrayIHandler\n" +
+			"n_onSuccess:(Ljava/lang/Object;)V:GetOnSuccess_Ljava_lang_Object_Handler:Android.Gms.Tasks.IOnSuccessListenerInvoker, Xamarin.GooglePlayServices.Tasks\n" +
+			"n_onFailure:(Ljava/lang/Exception;)V:GetOnFailure_Ljava_lang_Exception_Handler:Android.Gms.Tasks.IOnFailureListenerInvoker, Xamarin.GooglePlayServices.Tasks\n" +
+			"n_onComplete:(Lcom/google/android/gms/tasks/Task;)V:GetOnComplete_Lcom_google_android_gms_tasks_Task_Handler:Android.Gms.Tasks.IOnCompleteListenerInvoker, Xamarin.GooglePlayServices.Tasks\n" +
+			"";
+		mono.android.Runtime.register ("Upper_Clone.Activities.RegisterationActivity, Upper_Clone", RegisterationActivity.class, __md_methods);
+	}
+
+
+	public RegisterationActivity ()
+	{
+		super ();
+		if (getClass () == RegisterationActivity.class)
+			mono.android.TypeManager.Activate ("Upper_Clone.Activities.RegisterationActivity, Upper_Clone", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onRequestPermissionsResult (int p0, java.lang.String[] p1, int[] p2)
+	{
+		n_onRequestPermissionsResult (p0, p1, p2);
+	}
+
+	private native void n_onRequestPermissionsResult (int p0, java.lang.String[] p1, int[] p2);
+
+
+	public void onSuccess (java.lang.Object p0)
+	{
+		n_onSuccess (p0);
+	}
+
+	private native void n_onSuccess (java.lang.Object p0);
+
+
+	public void onFailure (java.lang.Exception p0)
+	{
+		n_onFailure (p0);
+	}
+
+	private native void n_onFailure (java.lang.Exception p0);
+
+
+	public void onComplete (com.google.android.gms.tasks.Task p0)
+	{
+		n_onComplete (p0);
+	}
+
+	private native void n_onComplete (com.google.android.gms.tasks.Task p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
